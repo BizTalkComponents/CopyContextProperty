b12cc25bee1b069be4e0bbb2689ede033b028043
robinhultman <robin.hultman@gmail.com>
2014-10-02 12:38:13 +0200

    Added new component for copying context properties

---
+++ CopyContextProperty/CopyContextProperty.Component.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace BizTalkComponents.CopyContextProperty
+{
+    public partial class CopyContextProperty
+    {
+        public string Name { get { return "CopyContextProperty."; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get { return "Copies one context property from another context property."; } }
+        
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("4C3DDF79-7FA6-4A2D-B750-8982F549FBE5");
+        }
+
+        public void InitNew()
+        {
+            
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return null;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+
+    }
+}
---
+++ CopyContextProperty/CopyContextProperty.cs
@@ -0,0 +1,51 @@
+﻿using BizTalkComponents.Utils.ContextPropertyHelpers;
+using BizTalkComponents.Utils.PropertyBagHelpers;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Guid = System.Runtime.InteropServices;
+namespace BizTalkComponents.CopyContextProperty
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [Guid.Guid("98EB6BA0-4FBC-44C5-8A53-A7D37C46A396")]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    public partial class CopyContextProperty : IComponent, IBaseComponent,
+                                        IPersistPropertyBag, IComponentUI
+    {
+        private const string SourcePropertyName = "SourceProperty";
+        private const string DestinationPropertyName = "DestinationPropertyName";
+
+        public string SourceProperty { get; set; }
+        public string DestinationProperty { get; set; }
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            var sourceContextProperty = new ContextProperty(SourceProperty);
+            var destinationContextProperty = new ContextProperty(DestinationProperty);
+
+            ContextPropertyHelper.CopyContextProperty(pInMsg, sourceContextProperty, destinationContextProperty);
+
+            return pInMsg;
+        }
+
+        
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            if (string.IsNullOrEmpty(SourceProperty))
+            {
+                SourceProperty = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, SourcePropertyName), string.Empty);
+            }
+
+            if (string.IsNullOrEmpty(DestinationProperty))
+            {
+                DestinationProperty = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, DestinationPropertyName), string.Empty);
+            }
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            PropertyBagHelper.WritePropertyBag(propertyBag, SourcePropertyName, SourceProperty);
+            PropertyBagHelper.WritePropertyBag(propertyBag, DestinationPropertyName, DestinationProperty);
+        }
+        
+    }
+}
