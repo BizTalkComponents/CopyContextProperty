66a4cefe7fb36d47c6aac04136238d707446a030
robinhultman <robin.hultman@gmail.com>
2014-10-02 14:52:44 +0200

    Added unit tests

---
+++ Tests/UnitTests/CopyContextPropertyTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Winterdom.BizTalk.PipelineTesting;
+
+namespace BizTalkComponents.CopyContextProperty.Tests.UnitTests
+{
+    [TestClass]
+    public class CopyContextPropertyTests
+    {
+        [TestMethod]
+        public void TestCopyProperty()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+            var component = new PipelineComponents.CopyContextProperty.CopyContextProperty
+            {
+                SourceProperty = "http://tempuri.org#Source",
+                DestinationProperty = "http://tempuri.org#Destination"
+            };
+
+            pipeline.AddComponent(component,PipelineStage.Decode);
+            
+            var message = MessageHelper.Create("<test></test>");
+            message.Context.Promote("Source","http://tempuri.org","Test");
+
+            Assert.IsNull(message.Context.Read("Destination", "http://tempuri.org"));
+
+            var output = pipeline.Execute(message);
+
+            Assert.AreEqual(1,output.Count);
+
+            Assert.IsTrue(output[0].Context.IsPromoted("Destination", "http://tempuri.org"));
+        }
+    }
+}
